6fd4aac78b92b5d2ab5c7bdd1169e8d668fed2f3
Bob Schmitz <14095796+rgschmitz1@users.noreply.github.com>
2021-01-07 11:01:48 -0800

    Removed animated_gears project
    Signed-off-by: Bob Schmitz <14095796+rgschmitz1@users.noreply.github.com>

--- animated_gears/timer.js
+++
@@ -1,8 +0,0 @@
-var mytimer = setInterval(myTimer, 1000);
-var countdown = 15;
-
-function myTimer() {
-    countdown--;
-    if (countdown == 0) clearInterval(mytimer)
-    console.log(countdown);
-}
